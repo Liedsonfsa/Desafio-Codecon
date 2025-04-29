6bdecf43550705c0ee9fcab5e764a7e450b54037
Liedsonfsa <fabricoliedson@gmail.com>
2025-04-29 10:01:33 -0300

    feat: adding route to returns superusers

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,11 @@ import (
 	"net/http"
 
 	"github.com/Liedsonfsa/Desafio-Codecon/internal/controllers"
-	// "github.com/Liedsonfsa/Desafio-Codecon/internal/models"
 )
 
 func main() {
 	http.HandleFunc("POST /users", controllers.SaveUsers)
+	http.HandleFunc("GET /superusers", controllers.GetSuperUsers)
 
 	http.ListenAndServe(":3000", nil)
 }
--- internal/controllers/users.go
+++ internal/controllers/users.go
@@ -32,3 +32,31 @@ func SaveUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&users)
 }
+
+func GetSuperUsers(w http.ResponseWriter, r *http.Request) {
+	var users []models.Users
+
+	file, err := os.Open("users.json")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&users)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var superusers []models.Users
+	for _, user := range users {
+		if user.Score >= 900 && user.Active {
+			superusers = append(superusers, user)
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&superusers)
+}
